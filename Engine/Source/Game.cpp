1659f9f6878ea59c485bc558935d8d6b479f8a78
chadmbradbury <chadmbradbury@gmail.com>
2012-02-17 05:31:24 +0000

    Working on Vector.h

--- Engine/Source/Game.cpp
+++ Engine/Source/Game.cpp
@@ -7,6 +7,8 @@
 #include "GameObject\GameObject.h"
 #include "Wrapper\AudioManager.h"
 
+// Included temporarily to make sure it is being built
+#include "Data Structures\Vector.h"
 
 #ifdef _DEBUG
 #include <iostream>
